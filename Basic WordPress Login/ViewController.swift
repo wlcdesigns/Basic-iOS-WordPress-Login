eb7098e0f5f9eb40db33791ab62ccf2b55fddaf4
wLc <wlc@wlc.local>
2015-08-11 15:10:49 -0700

    Initial Commit

---
+++ Basic WordPress Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic WordPress Login
+//
+//  Created by wlc on 8/11/15.
+//  Copyright (c) 2015 wLc Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Basic WordPress LoginTests/Basic_WordPress_LoginTests.swift
@@ -0,0 +1,36 @@
+//
+//  Basic_WordPress_LoginTests.swift
+//  Basic WordPress LoginTests
+//
+//  Created by wlc on 8/11/15.
+//  Copyright (c) 2015 wLc Designs. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Basic_WordPress_LoginTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
